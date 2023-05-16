25aecde6a0ced01986e9cdd169cc9d7d94c2154f
Choudhary Reyan Khan <93575436+ReyanChaudhary@users.noreply.github.com>
2023-05-16 16:58:43 +0530

    Add files via upload

---
+++ about.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About Me</title>
+</head>
+<body>
+    <h1>"This is About Me"</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quia commodi error dolor reiciendis autem dignissimos amet harum ipsam in delectus pariatur ab, sapiente aliquid! Voluptates praesentium libero exercitationem aliquam dolorem.</p>
+</body>
+</html>
